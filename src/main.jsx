d7155e6a24dbc4b33788c6cd14c42caf8a53854c
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-05-29 20:33:46 -0300

    :recycle: refactor: Material UI installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-
 import { Provider } from 'react-redux'
+
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import store from '@/store'
+import { theme } from '@/styles'
 import App from '@/pages/App'
-import '@/styles/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 )
